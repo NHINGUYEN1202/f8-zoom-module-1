525f29c4cc2c4aaceb00b7638752f7f361081229
Nguyễn Thị Yến Nhi <nguyenthiyennhi@MacBook-Pro-cua-Nguyen.local>
2025-05-19 14:26:03 +0900

    Add Blog

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
             input: {
                 index: path.resolve(__dirname, "src/index.html"),
                 about: path.resolve(__dirname, "src/about.html"),
+                blog: path.resolve(__dirname, "src/blog.html"),
                 // Thêm trang khác nếu có
             },
         },
